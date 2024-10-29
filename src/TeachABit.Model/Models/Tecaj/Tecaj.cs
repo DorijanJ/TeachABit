5b21f12edd0c318b56cd0becdc389c19132797e2
TomislavSesar <tomislav.sesar@fer.unizg.hr>
2024-10-29 20:20:33 +0100

    Klasa Tecaj

---
+++ src/TeachABit.Model/Models/Tecaj/Tecaj.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeachABit.Model.Models.Tecaj;
+[Table("Tecaj")]
+public class Tecaj
+{ 
+    [Key]
+    public int Id {get; set;}
+    public string Naziv {get; set;}
+}       
--- src/TeachABit.Model/TeachABitContext.cs
+++ src/TeachABit.Model/TeachABitContext.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TeachABit.Model.Models.Tecaj;
 using TeachABit.Model.Models.User;
 
 namespace TeachABit.Model
 {
     public class TeachABitContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
     {
+        public DbSet<Tecaj> Tecajevi {get; set;}
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
