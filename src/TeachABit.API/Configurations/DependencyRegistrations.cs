006207dd7a8eaa7af7c78bb4f0003884389483e5
MatejJurisic <matej.jurisic@fer.unizg.hr>
2024-11-08 18:40:28 +0100

    Popravio using

--- src/TeachABit.API/Configurations/DependencyRegistrations.cs
+++ src/TeachABit.API/Configurations/DependencyRegistrations.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using TeachABit.Model.Models.Korisnici;
 using TeachABit.Repository.Repositories.Objave;
+using TeachABit.Repository.Repositories.Radionice;
 using TeachABit.Repository.Repositories.Tecajevi;
 using TeachABit.Service.Services.Authentication;
 using TeachABit.Service.Services.Authorization;
-using TeachABit.Service.Services.Radionice;
 using TeachABit.Service.Services.Objave;
+using TeachABit.Service.Services.Radionice;
 using TeachABit.Service.Services.Tecajevi;
 using TeachABit.Service.Util.Mail;
 using TeachABit.Service.Util.Token;
