217a1bc80fa317a4cb699944179d22dccd6e5a53
MatejJurisic <matej.jurisic@fer.unizg.hr>
2024-11-09 22:05:17 +0100

    Dodao model state provjere

--- src/TeachABit.Model/DTOs/Authentication/ForgotPasswordDto.cs
+++ src/TeachABit.Model/DTOs/Authentication/ForgotPasswordDto.cs
@@ -1,7 +1,10 @@
-﻿namespace TeachABit.Model.DTOs.Authentication
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeachABit.Model.DTOs.Authentication
 {
     public class ForgotPasswordDto
     {
+        [EmailAddress(ErrorMessage = "Neispravan Email.")]
         public string Email { get; set; } = string.Empty;
     }
 }
--- src/TeachABit.Model/DTOs/Authentication/ResetPasswordDto.cs
+++ src/TeachABit.Model/DTOs/Authentication/ResetPasswordDto.cs
@@ -1,7 +1,10 @@
-﻿namespace TeachABit.Model.DTOs.Authentication
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeachABit.Model.DTOs.Authentication
 {
     public class ResetPasswordDto
     {
+        [EmailAddress(ErrorMessage = "Neispravan Email.")]
         public string Email { get; set; } = string.Empty;
         public string Token { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
