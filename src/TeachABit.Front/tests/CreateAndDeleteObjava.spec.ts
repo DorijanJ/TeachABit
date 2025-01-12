b82b60df2152976ec576fe4c5c9067d5fe5d493b
MatejJurisic <matej.jurisic@fer.unizg.hr>
2025-01-13 00:20:13 +0100

    Preimenovao test

--- src/TeachABit.Front/tests/CreateAndDeleteObjava.spec.ts
+++ src/TeachABit.Front/tests/CreateAndDeleteObjava.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, chromium } from "@playwright/test";
 import performLogin from "./helpers/login";
 import createDeleteObjava from "./helpers/createDeleteObjava";
 
-test("CreateObjava", async ({ browser }) => {
+test("CreateAndDeleteObjava", async ({ browser }) => {
     const context = await browser.newContext({ ignoreHTTPSErrors: true });
     const page = await context.newPage();
 
