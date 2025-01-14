002181f5fc37c0a9f80d113706ef1193f0dd0157
TomislavSesar <tomislav.sesar@fer.unizg.hr>
2025-01-14 15:49:29 +0100

    Promjenjeno ograničenje na UpdatedLekcijaDto

--- src/TeachABit.Model/DTOs/Tecajevi/UpdatedLekcijaDto.cs
+++ src/TeachABit.Model/DTOs/Tecajevi/UpdatedLekcijaDto.cs
@@ -2,12 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TeachABit.Model.DTOs.Tecajevi;
 
-public class UpdatedLekcija
+public class UpdatedLekcijaDto
 {
     public int Id { get; set; }
     [StringLength(100, ErrorMessage = "Naslov je previše dugačak.")]
     public string Naziv { get; set; } = string.Empty;
-    [StringLength(10000, ErrorMessage = "Sadržaj je previše dugačak.")]
+    [StringLength(30000, ErrorMessage = "Sadržaj je previše dugačak.")]
     public string Sadrzaj { get; set; } = string.Empty;
     
 }
