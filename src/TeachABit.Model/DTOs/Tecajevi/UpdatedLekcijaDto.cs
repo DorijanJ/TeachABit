4827f91cea1166743f6fe44d2e5dea32b40d0772
TomislavSesar <tomislav.sesar@fer.unizg.hr>
2025-01-14 15:45:44 +0100

    UpdatedLecijaDto model, dodano ograničenje u model LecijaDto te dodana lista lekcijadto u tecajdto

--- src/TeachABit.Model/DTOs/Tecajevi/UpdatedLekcijaDto.cs
+++ src/TeachABit.Model/DTOs/Tecajevi/UpdatedLekcijaDto.cs
@@ -2,11 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TeachABit.Model.DTOs.Tecajevi;
 
-public class LekcijaDto
+public class UpdatedLekcija
 {
     public int Id { get; set; }
     [StringLength(100, ErrorMessage = "Naslov je previše dugačak.")]
+    public string Naziv { get; set; } = string.Empty;
+    [StringLength(10000, ErrorMessage = "Sadržaj je previše dugačak.")]
     public string Sadrzaj { get; set; } = string.Empty;
-    [StringLength(30000, ErrorMessage = "Sadržaj je previše dugačak.")]
-    public DateTime CreatedDateTime { get; set; }
+    
 }
