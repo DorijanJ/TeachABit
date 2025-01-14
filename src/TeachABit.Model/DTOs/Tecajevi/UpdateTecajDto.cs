0edef49578a39c931a1592cb98e205e374096497
TomislavSesar <tomislav.sesar@fer.unizg.hr>
2025-01-14 11:23:28 +0100

    Dodano ogranicenje za sadrzaj dto modela tecaja

--- src/TeachABit.Model/DTOs/Tecajevi/UpdateTecajDto.cs
+++ src/TeachABit.Model/DTOs/Tecajevi/UpdateTecajDto.cs
@@ -7,5 +7,6 @@ public class UpdateTecajDto
     public int Id { get; set; }
     [StringLength(100, ErrorMessage = "Naslov je previše dugačak.")]
     public string Naziv { get; set; } = string.Empty;
+    [StringLength(30000, ErrorMessage = "Sadržaj je previše dugačak.")]
     public string Sadrzaj { get; set; } = string.Empty;
 }
