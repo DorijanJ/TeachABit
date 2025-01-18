5adb6cd71f2c6599b97709dff12b27e038659b38
MatejJurisic <matej.jurisic@fer.unizg.hr>
2025-01-18 01:08:56 +0100

    Pfp fix

--- src/TeachABit.Front/src/components/ImageUploadComponent.tsx
+++ src/TeachABit.Front/src/components/ImageUploadComponent.tsx
@@ -7,49 +7,66 @@ interface Props {
 }
 
 export default function ImageUploadComponent(props: Props) {
-
     const [file, setFile] = useState<File | null>();
 
     const image = useImage();
 
-    const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleFileChange = async (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
         if (event.target.files && event.target.files.length > 0) {
             const f = event.target.files[0];
             setFile(event.target.files[0]);
             props.setFile(await image.toBase64(f));
         }
-    }
+    };
 
-    return <>
-        <form style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-            <input
-                accept="image/*"
-                style={{ display: "none" }}
-                id="file-upload"
-                type="file"
-                onChange={handleFileChange}
-            />
-            <label htmlFor="file-upload">
-                <Button
-                    variant="outlined"
-                    component="span"
-                    color="primary"
-                >
-                    Odabir slike
-                </Button>
-            </label>
-            <span style={{ marginLeft: "10px" }}>
-                {file ? file.name : "No file chosen"}
-            </span>
-            {file && (
-                <img
-                    src={URL.createObjectURL(file)}
-                    style={{
-                        width: "60%",
-                        height: "60%",
-                    }}
+    return (
+        <>
+            <form
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "10px",
+                    alignItems: "center",
+                    maxWidth: "100%",
+                }}
+            >
+                <input
+                    accept="image/*"
+                    style={{ display: "none" }}
+                    id="file-upload"
+                    type="file"
+                    onChange={handleFileChange}
                 />
-            )}
-        </form>
-    </>
+                <label htmlFor="file-upload">
+                    <Button variant="outlined" component="span" color="primary">
+                        Odabir slike
+                    </Button>
+                </label>
+                <span
+                    style={{
+                        marginLeft: "10px",
+                        width: "90%",
+                        wordWrap: "break-word",
+                        textAlign: "center",
+                    }}
+                >
+                    {file ? file.name : ""}
+                </span>
+                {file && (
+                    <div>{(file.size / 1024 / 1024).toPrecision(3)} mb</div>
+                )}
+                {file && (
+                    <img
+                        src={URL.createObjectURL(file)}
+                        style={{
+                            width: "80%",
+                            objectFit: "cover",
+                        }}
+                    />
+                )}
+            </form>
+        </>
+    );
 }
--- src/TeachABit.Front/src/pages/profil/EditProfilDialog.tsx
+++ src/TeachABit.Front/src/pages/profil/EditProfilDialog.tsx
@@ -2,18 +2,16 @@ import { useState } from "react";
 
 import requests from "../../api/agent";
 import ImageUploadComponent from "../../components/ImageUploadComponent";
-import { Button } from "@mui/material";
+import { Button, Dialog } from "@mui/material";
 
 interface Props {
     onClose: () => void;
 }
 
 export default function EditProfilDialog(props: Props) {
-
     const [file, setFile] = useState<string>("");
 
     const handleSubmit = async () => {
-
         const response = await requests.postWithLoading(
             "account/update-korisnik",
             { profilnaSlikaBase64: file }
@@ -26,11 +24,29 @@ export default function EditProfilDialog(props: Props) {
 
     return (
         <>
-            <ImageUploadComponent
-                setFile={(file: string) => setFile(file)} />
-            <Button onClick={handleSubmit}>
-                {"Spremi"}
-            </Button>
+            <Dialog open onClose={props.onClose}>
+                <div
+                    style={{
+                        width: "500px",
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "20px",
+                        padding: "20px",
+                        alignItems: "center",
+                    }}
+                >
+                    <ImageUploadComponent
+                        setFile={(file: string) => setFile(file)}
+                    />
+                    <Button
+                        variant="contained"
+                        disabled={file === ""}
+                        onClick={handleSubmit}
+                    >
+                        {"Spremi"}
+                    </Button>
+                </div>
+            </Dialog>
         </>
     );
 }
