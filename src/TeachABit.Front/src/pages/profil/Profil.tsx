118992f71e409d5435af10d57cf971dbb32bb41b
MatejJurisic <matej.jurisic@fer.unizg.hr>
2025-01-24 21:59:39 +0100

    Profl fix

--- src/TeachABit.Front/src/pages/profil/Profil.tsx
+++ src/TeachABit.Front/src/pages/profil/Profil.tsx
@@ -259,7 +259,11 @@ export const Profil = () => {
                                     username={username ?? ""}
                                     onClose={() => {
                                         setIsOpenImageDialog(false);
-                                        window.location.reload();
+                                        if (globalStore.currentUser?.username) {
+                                            navigate(
+                                                `/profil/${globalStore.currentUser.username}`
+                                            );
+                                        }
                                     }}
                                 />
                             )}
